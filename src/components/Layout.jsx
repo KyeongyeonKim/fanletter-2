e38799087d8823f98e25130e7e3ad9f9eba23614
KY <rudyeon@naver.com>
2024-02-20 15:04:42 +0900

    [Feat] 3. Router 셋업

---
+++ src/components/Layout.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Header from "./Header";
+import { Link, Outlet } from "react-router-dom";
+import styled from "styled-components";
+
+function Layout() {
+  return (
+    <>
+      <StNav>
+        <Link to="/">HOME</Link>
+        <div>
+          <Link to="/profile">내 프로필</Link>
+          <Link to="/">로그아웃</Link>
+        </div>
+      </StNav>
+      <Outlet />
+    </>
+  );
+}
+
+export default Layout;
+
+const StNav = styled.nav`
+  padding: 20px;
+  display: flex;
+  justify-content: space-between;
+  background-color: #b5c0d0;
+`;
--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,14 +1,35 @@
+import Layout from "components/Layout";
 import Detail from "pages/Detail";
 import Home from "pages/Home";
+import Login from "pages/Login";
+import Profile from "pages/Profile";
+import { useSelector } from "react-redux";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 export default function Router() {
+  const isLogin = true;
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/detail/:id" element={<Detail />} />
-        <Route path="*" element={<Navigate replace to="/" />} />
+        {isLogin ? (
+          <Route element={<Layout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/detail/:id" element={<Detail />} />
+            <Route path="*" element={<Navigate replace to="/" />} />
+          </Route>
+        ) : (
+          <>
+            <Route path="/" element={<Navigate replace to="/login" />} />
+            <Route
+              path="/detail/:id"
+              element={<Navigate replace to="/login" />}
+            />
+            <Route path="/profile" element={<Navigate replace to="/login" />} />
+            <Route path="/login" element={<Login />} />
+          </>
+        )}
       </Routes>
     </BrowserRouter>
   );
